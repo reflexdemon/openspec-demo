02bad8d19202cea2bf45f8ece9e93e0602735d51
Venkateswara VP <venkateswara.vp@gmail.com>
2026-03-18 07:14:33 -0400

    Inital Commit

---
+++ src/main/java/io/vpv/spec/openspec_demo/OpenspecDemoApplication.java
@@ -0,0 +1,13 @@
+package io.vpv.spec.openspec_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenspecDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenspecDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/vpv/spec/openspec_demo/OpenspecDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.vpv.spec.openspec_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenspecDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
